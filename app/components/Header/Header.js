bb276928c2f658931739258a7d230a14b49d7438
Rafael Freire <rafael.freire@muchmore.digital>
2019-07-09 23:56:21 -0300

    Implementation components

---
+++ app/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Header = (props) => {
+  const { text, onPress } = props;
+  return (
+    <View style={styles.button}>
+
+    </View>
+  );
+};
+
+export default Header;
---
+++ app/components/Header/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../config/styles';
+
+export default StyleSheet.create({
+  header: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: colors.buttonBackground,
+    margin: 5,
+  }
+});
